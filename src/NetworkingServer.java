ac8ebafd5b91f6a96b527d5265ff878d10d1038b
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2024-05-08 11:03:35 +0200

    första commit, tar så lång till att kopiera allt :/

---
+++ src/NetworkingServer.java
@@ -0,0 +1,27 @@
+import java.net.*;
+import java.io.*;
+public class NetworkingServer {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket client;
+        int portnumber = 1987;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+        try{
+            server = new ServerSocket(portnumber);
+        }catch(IOException ie){
+            System.out.println("Cannot open socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " + server);
+        while(true) {
+            try{
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+            } catch (IOException ie){
+                System.out.println("ett lämpligt error meddelande");
+            }
+        }
+    }
+}
